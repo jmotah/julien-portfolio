75ce2d3e1ef9b829fa20c6c841e61be67c000635
Julien Motaharian <jmotaharian1@gmail.com>
2025-01-22 19:48:36 -0500

    removed unnecessary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-slate-blue': '#264653',
-        'teal-green': '#2A9D8F',
-        'golden-yellow': '#E9C46A',
-        'pale-orange': '#F4A261',
-        'sienna-orange': '#772F1A',
       },
     },
   },
