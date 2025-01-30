d3d5ae1f7633be7ebf334c77fb3696539f89632a
Julien Motaharian <jmotaharian1@gmail.com>
2025-01-30 15:40:39 -0500

    vercel analytical changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AnimatedRoutes from "./AnimatedRoutes";
+import { Analytics } from "@vercel/analytics/react";
 
 
 export default function App() {
