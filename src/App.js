b85b3177949aa9ce39234ab059d267153139bd58
Julien Motaharian <jmotaharian1@gmail.com>
2025-02-03 17:23:14 -0500

    addition of speed insights for vercel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AnimatedRoutes from "./AnimatedRoutes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <AnimatedRoutes />
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </Router>
   );
 }
